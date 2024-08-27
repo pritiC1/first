3261d7214ed5f601cd3d27de947acb75cc782c79
pritiC1 <pritichaugule44@gmail.com>
2024-08-27 14:44:26 +0530

    Add files via upload

---
+++ new.py
@@ -0,0 +1,10 @@
+n1=int(input("enter the first value:"))
+n2=int(input("enter the second value:"))
+n3=int(input("enter the third value:"))
+
+if(n1>n2)and(n1>n3):
+    print(n1,"is greater")
+if(n2>n1)and(n2>n3):
+    print(n2,"is greater")
+if(n3>n2)and(n3>n1):
+    print(n3,"is greater")        
